d64376570a32db4c74006fdef9025f297aecc805
commushy <49800805+commushy@users.noreply.github.com>
2019-04-28 18:43:57 +0900

    Update bonecloth.py

--- bonecloth.py
+++ bonecloth.py
@@ -11,7 +11,8 @@ bonelist=[]
 vidx = 0
 initial = True
 
-armature = "Armature" # change this to the name of your armature 
+# need update: get armature reference from selected bones
+armature = "Armature"
 
 for x in bpy.context.selected_pose_bones:
     bonelist.append(x)
@@ -30,6 +31,18 @@ for x in bpy.context.selected_pose_bones:
 print(bonelist)
 print(loclist)
 print(edgelist)
+
+vec1 = loclist[1]-loclist[0]
+vec2 = loclist[2]-loclist[1]
+vec3 = vec1.cross(vec2) #normal vector of first 2 edges
+vec4 = -vec3 #inverse of vec3
+
+print(vec1)
+print(vec2)
+print(vec3)
+print(vec4)
+
+
 
 msh = bpy.data.meshes.new(name="physmesh")
 msh.from_pydata(loclist, edgelist, [])
