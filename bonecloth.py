ca634ce0b7ec1e19d83e24e1b9d69e6ee056b3cd
commushy <49800805+commushy@users.noreply.github.com>
2019-04-30 18:45:08 +0900

    Add files via upload

--- bonecloth.py
+++ bonecloth.py
@@ -1,15 +1,12 @@
 # run in pose mode with bone chain selected
-# currently generate mesh only
+# currently generate edge only
 
 import bpy
 import bmesh
 from mathutils import Vector  
 
 loclist=[]
-loclist2=[]
-loclist3=[]
-
-facelist=[]
+edgelist=[]
 bonelist=[]
 vidx = 0
 initial = True
@@ -28,42 +25,27 @@ for x in bpy.context.selected_pose_bones:
         initial = False
     else:
         loclist.append(xtailloc)
+    edgelist.append([vidx,vidx+1])
+    vidx=vidx+1
     
 print(bonelist)
 print(loclist)
+print(edgelist)
 
 vec1 = loclist[1]-loclist[0]
 vec2 = loclist[2]-loclist[1]
 vec3 = vec1.cross(vec2) #normal vector of first 2 edges
+vec4 = -vec3 #inverse of vec3
 
 print(vec1)
 print(vec2)
 print(vec3)
+print(vec4)
 
-n = len(loclist)
-print(n)
 
-for vidx in range(n):
-    v=loclist[vidx]
-    print(v)
-    loclist2.append(v+vec3)
-    loclist3.append(v-vec3)
-    if vidx < n-1: 
-        facelist.append([vidx,vidx+1,vidx+n+1,vidx+n])
-        facelist.append([vidx,vidx+1,vidx+n*2+1,vidx+n*2])
-
-loclist4 = loclist + loclist2 + loclist3
-
-n2=len(loclist4)
-
-print(n2)
-print(loclist2)
-print(loclist3)
-print(loclist4)
-print(facelist)
 
 msh = bpy.data.meshes.new(name="physmesh")
-msh.from_pydata(loclist4, [], facelist)
+msh.from_pydata(loclist, edgelist, [])
 msh.update()
 
 obj = bpy.data.objects.new(name="physobj", object_data=msh)
